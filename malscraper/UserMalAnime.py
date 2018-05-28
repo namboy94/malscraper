822e8263bbb9af2aae4db7aeb5d2563f4d3e127a
Hermann Krumrey <hermann@krumreyh.com>
2018-05-28 17:23:42 +0200

    Fix license headers

--- malscraper/UserMalAnime.py
+++ malscraper/UserMalAnime.py
@@ -1,21 +1,21 @@
-"""
-Copyright 2017-2018 Hermann Krumrey
+"""LICENSE
+Copyright 2017 Hermann Krumrey <hermann@krumreyh.com>
 
-This file is part of mal-scraper.
+This file is part of malscraper.
 
-mal-scraper is free software: you can redistribute it and/or modify
+malscraper is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-mal-scraper is distributed in the hope that it will be useful,
+malscraper is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with mal-scraper.  If not, see <http://www.gnu.org/licenses/>.
-"""
+along with malscraper.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE"""
 
 from typing import List
 from datetime import datetime
